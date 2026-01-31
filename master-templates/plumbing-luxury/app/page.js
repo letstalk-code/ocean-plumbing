2dc9a8b5f2a3bb43813eb0d32d6e4a10ce879cdd
Devon Curry <labiffilmhouse@Devons-MacBook-Pro-2.local>
2026-01-30 23:05:01 -0500

    Simplify page.js to minimal working version

--- master-templates/plumbing-luxury/app/page.js
+++ master-templates/plumbing-luxury/app/page.js
@@ -1,22 +1,8 @@
-import LuxuryHero from '../components/LuxuryHero';
-import LuxuryDetails from '../components/LuxuryDetails';
-import BentoServices from '../components/BentoServices';
-import Footer from '../components/Footer';
-
-// TIP: In a real app, we would use a dynamic switch based on the client name.
 export default function Home() {
     return (
         <main style={{ backgroundColor: '#fff' }}>
-            {/* Loading the Luxury Plumbing Template */}
-            <LuxuryHero />
-            <LuxuryDetails />
-            <BentoServices />
-
-            {/* Reusing the universal footer */}
-            <Footer />
-
-            {/* Modern Serif Font for this template */}
-            <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400..900;1,400..900&display=swap" rel="stylesheet" />
+            <h1>Ocean Plumbing Services</h1>
+            <p>Welcome to our website.</p>
         </main>
     );
 }
