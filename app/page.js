90957743e975e090f406a1567dc40dc46d812e18
Devon Curry <labiffilmhouse@Devons-MacBook-Pro-2.local>
2026-01-30 23:14:12 -0500

    Build from root directory with plumbing-luxury template

--- app/page.js
+++ app/page.js
@@ -1,10 +1,7 @@
 import LuxuryHero from '../master-templates/plumbing-luxury/components/LuxuryHero';
 import LuxuryDetails from '../master-templates/plumbing-luxury/components/LuxuryDetails';
 import BentoServices from '../master-templates/plumbing-luxury/components/BentoServices';
-import Testimonials from '../master-templates/plumbing-luxury/components/Testimonials';
-import ServiceAreas from '../master-templates/plumbing-luxury/components/ServiceAreas';
 import Footer from '../master-templates/plumbing-luxury/components/Footer';
-
 
 export default function Home() {
     return (
@@ -12,13 +9,10 @@ export default function Home() {
             <LuxuryHero />
             <LuxuryDetails />
             <BentoServices />
-            <Testimonials />
-            <ServiceAreas />
-
             <Footer />
 
-            {/* Syne Font for this template */}
-            <link href="https://fonts.googleapis.com/css2?family=Syne:wght@400;700;800&display=swap" rel="stylesheet" />
+            {/* Modern Serif Font for this template */}
+            <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400..900;1,400..900&display=swap" rel="stylesheet" />
         </main>
     );
 }
