496d22f5926512b64b7d7841f9053625f569e747
Devon Curry <labiffilmhouse@Devons-MacBook-Pro-2.local>
2026-01-30 23:32:49 -0500

    Switch to HVAC professional template for homepage

--- app/page.js
+++ app/page.js
@@ -1,22 +1,26 @@
-import LuxuryHero from '../master-templates/plumbing-luxury/components/LuxuryHero';
-import LuxuryDetails from '../master-templates/plumbing-luxury/components/LuxuryDetails';
-import BentoServices from '../master-templates/plumbing-luxury/components/BentoServices';
-import Testimonials from '../master-templates/plumbing-luxury/components/Testimonials';
-import ServiceAreas from '../master-templates/plumbing-luxury/components/ServiceAreas';
-import Footer from '../master-templates/plumbing-luxury/components/Footer';
+import Hero from '../master-templates/hvac-professional/components/Hero';
+import About from '../master-templates/hvac-professional/components/About';
+import FeaturedServices from '../master-templates/hvac-professional/components/FeaturedServices';
+import Testimonials from '../master-templates/hvac-professional/components/Testimonials';
+import ServiceAreas from '../master-templates/hvac-professional/components/ServiceAreas';
+import Team from '../master-templates/hvac-professional/components/Team';
+import Footer from '../master-templates/hvac-professional/components/Footer';
+import Navigation from '../master-templates/hvac-professional/components/Navigation';
 
 export default function Home() {
     return (
         <main style={{ backgroundColor: '#fff' }}>
-            <LuxuryHero />
-            <LuxuryDetails />
-            <BentoServices />
+            <Navigation />
+            <Hero />
+            <About />
+            <FeaturedServices />
             <Testimonials />
+            <Team />
             <ServiceAreas />
             <Footer />
 
-            {/* Modern Serif Font for this template */}
-            <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400..900;1,400..900&display=swap" rel="stylesheet" />
+            {/* Inter and Outfit Fonts */}
+            <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Outfit:wght@300;400;600;700&display=swap" rel="stylesheet" />
         </main>
     );
 }
